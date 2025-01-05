74612c20c8325658bac09ad2a4aa84f4f59058be
Aniket Kumar <98268675+way2aniiket@users.noreply.github.com>
2025-01-05 12:16:48 +0530

    Delete first-react-app directory

--- first-react-app/src/ReusableComponent.jsx
+++
@@ -1,8 +0,0 @@
-function ReusableComponent() {
-    let randomNumber = Math.random() * 100
-    return <>
-    <p>Random number is: {Math.round(randomNumber)}</p>
-    </>
-}
-
-export default ReusableComponent;
